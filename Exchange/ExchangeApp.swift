ddf759b3693fbcbb7e10de8241037d8c425602e4
Ashmeyn <danilleongard@gmail.com>
2026-07-18 14:05:17 +0300

    Lesson 1: project setup, SwiftUI + MVVM skeleton

---
+++ Exchange/ExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeApp.swift
+//  Exchange
+//
+//  Created by Daniil Leongard on 14.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
